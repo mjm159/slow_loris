c66a8939522d2b63da76849fcea38f7afb711e65
Michael Malocha <mjmaloch@us.ibm.com>
2016-11-12 19:57:09 -0500

    Update the bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,6 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+touch /var/www/index.html
+echo '<html><head></head><body>Hello, World!</body></html>' >> /var/www/index.html
+service apache2 reset
